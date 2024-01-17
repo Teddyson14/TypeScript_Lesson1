588397e21edc24cf63c2611bc6a8b47d832b7dea
Teddyson14 <128144405+Teddyson14@users.noreply.github.com>
2024-01-17 16:02:10 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// let arr = [1, 2, 3, 4, 5]
-
-// let func = () => {
-//     let i = 0;
-//     for(i; i < arr.length; i++){
-//         console.log(i)
-//     }
-// }
-
-// func;
-
-
 const array = [1, 2, 3, 4, 5];
 
 const printArrayElements = () => {
