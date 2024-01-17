435a8889eb0db22b9a958530772de1f0129edb47
Teddyson14 <128144405+Teddyson14@users.noreply.github.com>
2024-01-17 15:59:10 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// let arr = [1, 2, 3, 4, 5]
+
+// let func = () => {
+//     let i = 0;
+//     for(i; i < arr.length; i++){
+//         console.log(i)
+//     }
+// }
+
+// func;
+
+
+const array = [1, 2, 3, 4, 5];
+
+const printArrayElements = () => {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+};
+
+printArrayElements();
