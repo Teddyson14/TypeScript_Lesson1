84152472139ee97fd90dcf2c07792a9a47130f7b
Teddyson14 <mishgan1410@gmail.com>
2024-02-20 14:36:04 +1000

    Update script.ts

--- script.ts
+++ script.ts
@@ -24,30 +24,84 @@
 // console.log(rectSquare)
 
 // ------------------------------------------------------------------------------------------
-class Point {
-    x: number;
-    y: number;
-    z: number;
+// class Point {
+//     x: number;
+//     y: number;
+//     z: number;
 
-    constructor(x: number, y: number, z: number) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+//     constructor(x: number, y: number, z: number) {
+//         this.x = x;
+//         this.y = y;
+//         this.z = z;
+//     }
+// }
+
+// class Triangle {
+//     point1: Point;
+//     point2: Point;
+//     constructor(x1: number = 0, x2: number = 0, y1: number = 0, y2: number = 0, z1: number = 0, z2: number = 0) {
+//         this.point1 = new Point(x1, y1, z1)
+//         this.point2 = new Point(x2, y2, z2)
+//     }
+// }
+// let trilage1 = new Triangle()
+// let trilage2 = new Triangle(10, 15, 24, 42 ,32 ,9)
+
+// console.log("Trilage1:")
+// console.log(trilage1.point2.x)
+// console.log(trilage1.point2.y)
+// console.log(trilage1.point2.z)
+
+// -------------------------------------------------------------------------------------------------------------------
+
+class General {
+    name: string;
+    sizes: string;
+    price: number;
+}
+
+class Chair extends General {
+    legCount: number;
+    legHeight: number;
+    backrest: boolean;
+    constructor(_name: string, _sizes: string, _price: number) {
+        super()
+        this.name = _name;
+        this.sizes = _sizes;
+        this.price = _price;
     }
 }
 
-class Triangle {
-    point1: Point;
-    point2: Point;
-    constructor(x1: number = 0, x2: number = 0, y1: number = 0, y2: number = 0, z1: number = 0, z2: number = 0) {
-        this.point1 = new Point(x1, y1, z1)
-        this.point2 = new Point(x2, y2, z2)
+class Closet extends General {
+    handleMaterial: string;
+    leafNumber: number;
+    lockers: number;
+    constructor(_name: string, _sizes: string, _price: number) {
+        super()
+        this.name = _name;
+        this.sizes = _sizes;
+        this.price = _price;
     }
 }
-let trilage1 = new Triangle()
-let trilage2 = new Triangle(10, 15, 24, 42 ,32 ,9)
 
-console.log("Trilage1:")
-console.log(trilage1.point2.x)
-console.log(trilage1.point2.y)
-console.log(trilage1.point2.z)
+class Shelves extends General {
+    numberSegments: number;
+    sizeSegment: number;
+    constructor(_name: string, _sizes: string, _price: number) {
+        super()
+        this.name = _name;
+        this.sizes = _sizes;
+        this.price = _price;
+    }
+}
+
+class Tables extends General {
+    legCount: number;
+    worktopArea: number;
+    constructor(_name: string, _sizes: string, _price: number) {
+        super()
+        this.name = _name;
+        this.sizes = _sizes;
+        this.price = _price;
+    }
+}
